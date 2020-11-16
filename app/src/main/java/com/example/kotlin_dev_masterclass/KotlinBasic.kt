302d18d5e440a7d99f2a97bd3397141063f25543
NanthawatKTY <nanthawat.tae123@gmail.com>
2020-11-16 16:38:07 +0700

    Initial commit 1st

---
+++ app/src/main/java/com/example/kotlin_dev_masterclass/KotlinBasic.kt
@@ -0,0 +1,56 @@
+package com.example.kotlin_dev_masterclass
+
+fun main(){
+
+    // TODO: Add new function
+
+    val name = "Tae Young"
+
+    // type inference finds out the type from context
+    var myAge = 22
+
+    // Integer TYPES: Byte (8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
+    var myFloat: Float = 13.37F
+    val myPi: Double = 3.14159265358979
+    var age: Byte = 25
+    var year: Short = 2020
+    var myLong: Long = 18881234567
+
+    // Floating point number types: Float(32 bit), Double (64 bit)
+    // val myFloat: Float = 22.11F
+    // val myDouble: Double = 3.142323523523
+
+    // Boolean
+    var isRain: Boolean = true
+//    var isRain: Boolean = true;
+//    isRain = false
+
+    // Characters
+    val letterChar: Char = 'a'
+    val digitChar = '$'
+
+    //String
+    var firstCharInStr = name[0]
+    var lastCharInStr = name[name.length - 1] // Count in Array Style
+    var countCharInStr = name.length
+
+    //println("1st Character " + firstCharInStr + ' ' + lastCharInStr)
+    //println("Character Length: " + countCharInStr)
+
+
+    //Arithmetic operators (+, -, *, /, %)
+    var result = 5+3
+    val a = 5.0
+    val b = 3.0
+    var resultTwo : Double
+    resultTwo = a / b
+    result /= 2
+    result *= 3
+    result += 10
+    result -= 5
+    result %= 2  //Modulo (Mod)
+    println(resultTwo)
+
+    //Comparision Operators (==, >=, <=, !=, >, <)
+    
+}
